cabdc36f4faf8d36127fd5f65a54cc5bc444f635
Yerden II, Gerald <Gerald.Yerden@duke-energy.com>
2019-01-07 18:21:12 -0500

    added CORS to energyAssets service

--- functions/routes/energyAssets.js
+++ functions/routes/energyAssets.js
@@ -1,8 +1,15 @@
 const express = require("express")
+const cors = require("cors")
+
 const energyAsset = express()
+energyAsset.use(cors({ origin: true }))
 
 energyAsset.get("/energyAssets", (request, response) => {
-  response.send("Here is your energy asset!")
+  response.send('GET energyAssets')
+})
+
+energyAsset.post("/energyAssets", (request, response) => {
+  response.send(request.body)
 })
 
 module.exports = energyAsset
